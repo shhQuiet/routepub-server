d2b0551c4984c89315585422074b094ade1f042c
shhQuiet <stevehorne@gmail.com>
2014-04-01 23:33:47 -0500

    added config with nconf

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-var faye = require('faye');
+var fs = require('fs'),
+	nconf = require('nconf'),
+	server = require('./routepub-server.js');
 
+//
+// Setup nconf to use (in-order):
+//   1. Command-line arguments
+//   2. Environment variables
+//   3. A file located at 'path/to/config.json'
+//
+nconf.argv()
+	.env()
+	.file({
+		file: 'config.json'
+	});
+
+console.log("======== Starting server ========\nUsing port    [" + nconf.get("PORT") + "]");
+
+server.start(nconf);
