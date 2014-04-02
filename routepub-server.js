b1df9390dfedffc6ad8d0f949dab10bb368f4d80
shhQuiet <stevehorne@gmail.com>
2014-04-02 00:18:57 -0500

    getInfo

--- routepub-server.js
+++ routepub-server.js
@@ -1,12 +1,21 @@
 var http = require('http'),
-	faye = require('faye');
+	faye = require('faye'),
+	express = require('express'),
+	config;
 
-exports.start = function(config) {
-	var server = http.createServer(),
-		bayeux = new faye.NodeAdapter(config.get('faye.NodeAdapter')),
-		port = config.get('PORT');
-
-	bayeux.attach(server);
-	server.listen(port);
-	console.log("server is listening at port " + port)
+function getInfo(req, res, next) {
+	res.send(200, '<h1>Your info sir</h1>');
 }
+
+exports.start = function(cfg) {
+	var bayeux = new faye.NodeAdapter(cfg.get('faye.NodeAdapter')),
+		port = cfg.get('PORT'),
+		app = express();
+	config = cfg;
+
+	bayeux.attach(app);
+	app.listen(port);
+	console.log("server is listening at port " + port);
+
+	app.get('/info', getInfo);
+};
